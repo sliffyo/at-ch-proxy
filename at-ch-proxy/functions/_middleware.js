bc12abac49c672dc84c04e91b097269e3d7e8a6f
sliffyo <holger.saxler@gmail.com>
2025-06-02 15:43:14 +0200

    Delete at-ch-proxy directory

--- at-ch-proxy/functions/_middleware.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  url.hostname = "einfach-gefunden.de";
-
-  const modifiedRequest = new Request(url.toString(), context.request);
-  return fetch(modifiedRequest);
-}
